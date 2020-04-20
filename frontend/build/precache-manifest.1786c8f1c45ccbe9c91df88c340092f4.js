87675571f02ebef727e577bb71c504902d4e506e
Jesús Moris <jesusmoriis@hotmail.com>
2020-04-20 01:33:17 -0400

    se sube archivo build de produccion

---
+++ frontend/build/precache-manifest.1786c8f1c45ccbe9c91df88c340092f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66592fa4777a9602f475591a5e62c547",
+    "url": "/static/media/logo_light.66592fa4.svg"
+  },
+  {
+    "revision": "6cd04c4e80726f49e43dd72e2a0ac2c8",
+    "url": "/static/media/logo_dark.6cd04c4e.svg"
+  },
+  {
+    "revision": "b6de3c1a9121c14fd4ae",
+    "url": "/static/js/runtime~main.b6de3c1a.js"
+  },
+  {
+    "revision": "077d028917020211878b",
+    "url": "/static/js/main.077d0289.chunk.js"
+  },
+  {
+    "revision": "5e889f0a7255885537cc",
+    "url": "/static/js/2.5e889f0a.chunk.js"
+  },
+  {
+    "revision": "077d028917020211878b",
+    "url": "/static/css/main.349598ca.chunk.css"
+  },
+  {
+    "revision": "5e889f0a7255885537cc",
+    "url": "/static/css/2.83dc3400.chunk.css"
+  },
+  {
+    "revision": "af2762f34ae9b51d05186c37a4b9976d",
+    "url": "/index.html"
+  }
+]);
