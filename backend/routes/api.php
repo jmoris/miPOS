ce83ab176cdf6986eade26ae30700af6fcdceb49
Jesús Moris <jesusmoriis@hotmail.com>
2020-04-19 22:49:17 -0400

    se implemento auth jwt básico

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +12,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'AuthController@logout');
 });
